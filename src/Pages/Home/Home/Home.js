512a80db37aaa43b4e57f479b728759b45ffee6d
Shidul Islam <sishahin093@gmail.com>
2022-04-15 00:53:21 +0600

    60-3 Collect, Edit, optimize and host images for your website

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Serviecs from '../Seviecs/Serviecs';
+
+const Home = () => {
+    return (
+        <div>
+            <Serviecs>
+
+            </Serviecs>
+        </div>
+    );
+};
+
+export default Home;
