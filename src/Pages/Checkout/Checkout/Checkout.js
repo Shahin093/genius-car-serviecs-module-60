ab6a89dcc5510ec5f4f61f5cb1be58e3859da4fa
Shidul Islam <sishahin093@gmail.com>
2022-04-16 09:03:54 +0600

    62-1 Module overview and social login initial setup

---
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Checkout = () => {
+    return (
+        <div>
+            <h2>THis is CHeckout out Lish</h2>
+        </div>
+    );
+};
+
+export default Checkout;
